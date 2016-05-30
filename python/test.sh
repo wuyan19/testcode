f4a9145965aa5eec3359f6cc252a5f241e89d059
wuyan19 <wuyan637419@163.com>
2016-05-29 22:13:00 -0700

    add folder (python) and file(test.sh)

---
+++ python/test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+
+# game loop
+while true; do
+    maxH=0
+    index=0
+    for (( i=0; i<8; i++ )); do
+        # mountainH: represents the height of one mountain, from 9 to 0.
+        read mountainH
+        if [ $mountainH -gt $maxH ];then
+            maxH=$mountainH
+	    index=$i
+        fi
+    done
+
+    # Write an action using echo
+    # To debug: echo "Debug messages..." >&2
+
+    echo $index # The number of the mountain to fire on.
+done
